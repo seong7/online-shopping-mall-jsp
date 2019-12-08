80b31d0d8576923c7ce0c993f8a54c57a9802633
Seongjin <seongjinkim1123@gmail.com>
2019-12-08 23:24:28 +0900

    20191208 | 회원가입 폼 작업중

--- WebContent/vscode__utf8/js/top.js
+++ WebContent/vscode__utf8/js/top.js
@@ -1,11 +1,17 @@
 function init(){
+	
+    //search ajax
+    searchInput();
+    inputTransition();
+    
 
     /******************  모달 제어부  *******************/
-    
+
     let modaltrigger = false;
     const userCheckBox = document.querySelector('#userPopup');
     const modalWindow = document.getElementById('modalDiv');
     const modalInnerFrame = document.getElementById('modalInnerFrame');
+
     const modal_elements_wrapper = document.getElementById('modal_elements_wrapper');
     const login_form = document.getElementById('login_form');
 
@@ -15,6 +21,7 @@ function init(){
     modalInnerFrame.style.height = "0px";
     userCheckBox.addEventListener('change', function(){
         if(!modaltrigger){
+
             modaltrigger = true;
             // console.log(modaltrigger);
             modalWindow.style.display = "block";
@@ -50,6 +57,10 @@ function init(){
             modalInnerFrame.style.height = "0px";
             // modal_elements_wrapper.style.opacity="0";
             // modal_elements_wrapper.style.transition="opacity 0.3s";
+            
+            setTimeout(function(){
+                login_signup('login');
+            }, 600);
         }
     });
 
@@ -99,4 +110,63 @@ function login_signup(id){
 };
 
 
-  init();
+
+
+
+function searchInput(){
+    $("#search_input").autocomplete({ 
+            source : function( request, response ) {
+                 const autoValues = $.ajax({
+                     url: "search.jsp", 
+                     dataType: "json",
+                      data: {
+                          searchValue: request.term
+                         },
+                          success: function(result) { 
+                                response( 
+                                   $.map( result, function(item) {
+                                        return { 
+                                            label: item.data,
+                                            value: item.data 
+                                        }
+                                   })
+                                );
+                          }
+                 });
+        },  
+        minLength: 1, 
+        select: function( event, ui ) {
+        	
+        } 
+   }).focus(function(){            
+       // The following works only once.
+       // $(this).trigger('keydown.autocomplete');
+       // As suggested by digitalPBK, works multiple times
+       // $(this).data("autocomplete").search($(this).val());
+       // As noted by Jonny in his answer, with newer versions use uiAutocomplete
+       $(this).data("uiAutocomplete").search($(this).val());
+   });
+    
+    $.ui.autocomplete.prototype._renderItem = function (ul, item) {
+        item.label = item.label.replace(new RegExp("(?![^&;]+;)(?!<[^<>]*)(" + $.ui.autocomplete.escapeRegex(this.term) + ")(?![^<>]*>)(?![^&;]+;)", "gi"), "<strong>$1</strong>");
+        return $("<li></li>")
+                .data("item.autocomplete", item)
+                .append("<a>" + item.label + "</a>")
+                .appendTo(ul);
+    };
+}
+
+function inputTransition(){
+    const input = document.querySelector('#search_input');
+    let searchIcon = document.querySelector('.fa-search');
+
+    input.addEventListener('focus', function(){
+        searchIcon.style.color = "#ff9f43";
+    });
+    input.addEventListener("focusout", function(){
+        searchIcon.style.color = "#a4b0be";
+    });
+}
+
+init();
+
