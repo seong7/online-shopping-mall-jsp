276cdc2eee6a1c63d17b000cc579126e7c1958ab
zboizz <zboizz@hanmail.net>
2019-12-17 18:18:11 +0900

    20191217 | producting

--- src/Product/ProductMgr.java
+++ src/Product/ProductMgr.java
@@ -80,13 +80,157 @@ public class ProductMgr {
 			pool.freeConnection(con, pstmt, rs);
 		}
 		return vlist;
+	}		
+	//베스트상품 리스트
+	public Vector<ProductBean> getBestList(){
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = null;
+		Vector<ProductBean> blist = new Vector<ProductBean>();		
+		try {
+			con = pool.getConnection();
+			sql = "SELECT p_name, p_price, p.p_main_pht_name "
+				+ "FROM product_mst_tb p JOIN stock_tb s ON p.p_code = s.p_code "
+				+ "GROUP BY p.p_code ORDER BY (SUM(s.st_enter_qty)-SUM(s.st_ava_qty)) DESC";
+			pstmt = con.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				ProductBean bean = new ProductBean();				
+				bean.setP_name(rs.getString(1));
+				bean.setP_price(rs.getInt(2));
+				bean.setP_main_pht_name(rs.getString(3));
+				blist.addElement(bean);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			pool.freeConnection(con, pstmt, rs);
+		}
+		return blist;
+	}
+	//신상품
+	public Vector<ProductBean> getNewList(){
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = null;
+		Vector<ProductBean> nlist = new Vector<ProductBean>();		
+		try {
+			con = pool.getConnection();
+			sql = "SELECT p_name, p_price, p_main_pht_name "
+				+ "FROM product_mst_tb WHERE p_date ORDER BY p_date DESC";
+			pstmt = con.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				ProductBean bean = new ProductBean();				
+				bean.setP_name(rs.getString(1));
+				bean.setP_price(rs.getInt(2));
+				bean.setP_main_pht_name(rs.getString(3));
+				nlist.addElement(bean);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			pool.freeConnection(con, pstmt, rs);
+		}
+		return nlist;
 	}	
 	
-	//reviewlist
-	//priceuplist
-	//pricedownlist
-	//searchlist
-	//product insert
+	//리뷰많은 상품 리스트
+	public Vector<ProductBean> getReviewList(){
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = null;
+		Vector<ProductBean> rlist = new Vector<ProductBean>();		
+		try {
+			con = pool.getConnection();
+			sql = "SELECT p.p_name, p.p_price, p.p_main_pht_name "
+				+	"FROM product_mst_tb p JOIN review_tb r ON p.p_code = r.p_code "
+				+	"GROUP BY p.p_code ORDER BY COUNT(r.p_code) DESC";
+			pstmt = con.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				ProductBean bean = new ProductBean();				
+				bean.setP_name(rs.getString(1));
+				bean.setP_price(rs.getInt(2));
+				bean.setP_main_pht_name(rs.getString(3));
+				rlist.addElement(bean);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			pool.freeConnection(con, pstmt, rs);
+		}
+		return rlist;
+	}
+	//가격높은순
+	public Vector<ProductBean> getHighList(){
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = null;
+		Vector<ProductBean> phlist = new Vector<ProductBean>();		
+		try {
+			con = pool.getConnection();
+			sql = "SELECT p_name, p_price, p_main_pht_name "
+				  + "FROM product_mst_tb ORDER BY p_price DESC";
+			pstmt = con.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				ProductBean bean = new ProductBean();				
+				bean.setP_name(rs.getString(1));
+				bean.setP_price(rs.getInt(2));
+				bean.setP_main_pht_name(rs.getString(3));
+				phlist.addElement(bean);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			pool.freeConnection(con, pstmt, rs);
+		}
+		return phlist;
+	}
+	//pricelowlist
+	public Vector<ProductBean> getLowList(){
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = null;
+		Vector<ProductBean> pllist = new Vector<ProductBean>();		
+		try {
+			con = pool.getConnection();
+			sql = "SELECT p_name, p_price, p_main_pht_name "
+				+ "FROM product_mst_tb	ORDER BY p_price";
+			pstmt = con.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				ProductBean bean = new ProductBean();				
+				bean.setP_name(rs.getString(1));
+				bean.setP_price(rs.getInt(2));
+				bean.setP_main_pht_name(rs.getString(3));
+				pllist.addElement(bean);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			pool.freeConnection(con, pstmt, rs);
+		}
+		return pllist;
+	}
+	//상품검색
+	
 	
 
 }
